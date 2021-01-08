c8303462e6a372f83b01a5364c5e9a96c8fc0308
AD58-3104 <19c1012sk@s.chibakoudai.jp>
2021-01-08 09:09:56 +0000

    add * substitute for x

--- scripts/motor_sub.py
+++ scripts/motor_sub.py
@@ -7,7 +7,7 @@ from ros_raspi_motor_control.msg import raspi_servo
 global servo
 
 def callback(data):
-    duty_cycle =(2.5 + (( 12.0 - 2.5 ) / 180.0) × ( data.degree + 90.0 ))
+    duty_cycle =(2.5 + (( 12.0 - 2.5 ) / 180.0) * ( data.degree + 90.0 ))
     servo.ChangeDutyCycle(duty_cycle)
     time.sleep(0.4)
     servo.ChangeDutyCycle(0.0)
