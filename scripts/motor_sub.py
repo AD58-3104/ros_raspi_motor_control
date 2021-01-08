5ea2a39911fc57475e07145d313aca46d95029c3
AD58-3104 <19c1012sk@s.chibakoudai.jp>
2021-01-08 11:20:10 +0000

    use pinNumber for setup

--- scripts/motor_sub.py
+++ scripts/motor_sub.py
@@ -5,14 +5,28 @@ import rospy
 from ros_raspi_motor_control.msg import raspi_servo
 
 global servo
+global first
 
 def callback(data):
-    duty_cycle =(2.5 + (( 12.0 - 2.5 ) / 180.0) * ( data.degree + 90.0 ))
-    servo.ChangeDutyCycle(duty_cycle)
-    time.sleep(0.4)
-    servo.ChangeDutyCycle(0.0)
+    global first
+    global servo
+    if first:
+        first = False
+        motor_pin_pwm = data.pinNumber 
+        GPIO.setup(motor_pin_pwm, GPIO.OUT)
+        servo = GPIO.PWM(motor_pin_pwm, 50)
+        servo.start(0)
+        duty_cycle =(2.5 + (( 12.0 - 2.5 ) / 180.0) * ( 0 + 90.0 ))
+        servo.ChangeDutyCycle(duty_cycle)
+        time.sleep(0.5)
+        servo.ChangeDutyCycle(0.0)
+    else:
+        duty_cycle =(2.5 + (( 12.0 - 2.5 ) / 180.0) * ( data.degree + 90.0 ))
+        servo.ChangeDutyCycle(duty_cycle)
+        time.sleep(0.4)
+        servo.ChangeDutyCycle(0.0)
 
-def listener():
+def sub():
 
     rospy.init_node('motor_sub', anonymous=True)
 
@@ -24,16 +38,12 @@ def listener():
         
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
-    motor_pin_pwm = 12 
-    GPIO.setup(motor_pin_pwm, GPIO.OUT)
-    servo = GPIO.PWM(motor_pin_pwm, 50)
-    servo.start(0)
-    duty_cycle =(2.5 + (( 12.0 - 2.5 ) / 180.0) * ( 0 + 90.0 ))
-    servo.ChangeDutyCycle(duty_cycle)
-    time.sleep(1)
-    servo.ChangeDutyCycle(0.0)
+    global first 
+    first = True
 
-    listener()
+
+
+    sub()
 
     servo.stop()
     GPIO.cleanup()
